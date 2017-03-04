85b5216bc62e2b4562f2ef9bc54d67d7ec232d3d
Brandon Stansbury <brandon.stansbury@infospace.com>
2017-03-03 17:22:00 -0800

    add async variations

--- server.py
+++ server.py
@@ -42,22 +42,47 @@ def serial():
     return json.dumps(stats, sort_keys=True, indent=2)
 
 
-@application.route('/concurrent')
-def concurrent():
-    req_args = {}
-    if 'timeout' in request.args:
-        req_args['timeout'] = float(request.args['timeout'])
+@application.route('/gather')
+def gather():
+    return _concurrent(_gather_requests)
+
+
+@application.route('/async')
+def async():
+    return _concurrent(_invoke_requests)
+
+
+def _concurrent(async_op):
     stats = {}
     start = time.time()
-    futures = [g.loop.run_in_executor(None, functools.partial(_call_request, i, req, **req_args)) for i, req in enumerate(reqs)]
-    res = g.loop.run_until_complete(asyncio.gather(*futures, return_exceptions=True))
+    res = g.loop.run_until_complete(async_op())
     for i, result in enumerate(res):
         stats['req' + str(i)] = str(result)
     stats['total'] = time.time() - start
     return json.dumps(stats, sort_keys=True, indent=2)
 
 
-def _call_request(index, *args, **kwargs):
+def _gather_requests():
+    co_requests = _wrap_requests()
+    return asyncio.gather(*co_requests, return_exceptions=True)
+
+
+async def _invoke_requests():
+    co_requests = _wrap_requests()
+    results = []
+    for i, c in enumerate(co_requests):
+        results.insert(i, await c)
+    return results
+
+
+def _wrap_requests():
+    req_args = {}
+    if 'timeout' in request.args:
+        req_args['timeout'] = float(request.args['timeout'])
+    return [g.loop.run_in_executor(None, functools.partial(_call_request, req, **req_args)) for req in reqs]
+
+
+def _call_request(*args, **kwargs):
     start = time.time()
     resp = requests.get(*args, **kwargs)
     return time.time() - start
